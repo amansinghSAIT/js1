b9c1d3bc76ec94fd6b612268af428949c817dcc2
amansinghSAIT <118374868+amansinghSAIT@users.noreply.github.com>
2026-04-13 10:41:30 +0000

    conversionof datatypes

---
+++ basic/03_conversionoperation.js
@@ -0,0 +1,23 @@
+let score = 33
+
+console.log(typeof score);
+
+let numvalue = Number(score); //now it sure it is a number
+
+// "33" = 33
+// "33abc = nan"
+//"true = 1" , "false = 0"
+
+
+let logged = 1;
+
+let bool = Boolean(logged);
+console.log( bool)
+
+// "" = false
+// "aman" = true
+
+let l =33
+let solle = String(l)
+console.log(solle)
+console.log(typeof solle)
