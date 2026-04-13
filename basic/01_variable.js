1038896a6d522bae568c54dbe76ac5282f92bbac
amansinghSAIT <118374868+amansinghSAIT@users.noreply.github.com>
2026-04-13 09:09:11 +0000

    datypes and variable logic

---
+++ basic/01_variable.js
@@ -0,0 +1,12 @@
+const accountID = 1
+let accountemail = "aman@gmail.com"
+var accountpass = "1234"
+accoutncity = "dhanabd"
+
+/*
+- prefer not to use var keyword because of problem in scope
+- when i have to print lot of output use cosnole.table([]) like this to print.
+- when we defined a variable in js and we don't assign any value it will be undefined.
+*/
+
+console.table([accountID,accountemail,accountpass])
---
+++ basic/02_datatypes.js
@@ -0,0 +1,7 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello"); we are using node.js not browser
+
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null); // object
